209f1037f4724a9416302dc5ca20b2fa58855647
Arthur Rocha Amaral <ruhtraamaral@hotmail.com>
2020-11-25 18:03:02 -0300

    reset

--- Procfile
+++
@@ -1,3 +0,0 @@
-// Procfile
-// release: yarn install-all && yarn --cwd client/ build && node movePublic.js && yarn --cwd api/ node ace migration:run --force
-web: npm run --prefix api/ start
--- client/src/components/masked-inputs/CPFInput.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import InputMask from './InputMask'
-
-function CPFInput(props) {
-  /* eslint react/jsx-props-no-spreading: 0 */
-  return <InputMask {...props} type="tel" format="###.###.###-##" placeholder="000.000.000-00" />
-}
-
-export default CPFInput
